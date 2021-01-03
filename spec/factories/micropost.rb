432b459b49f87fbde9ce5b2c43fa895612d829b4
kamada-maki <ikam.adamak17@gmail.com>
2021-01-04 00:32:33 +0900

    rubocopによる修正

--- spec/factories/micropost.rb
+++ spec/factories/micropost.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :micropost do
     association :user
-    content   {Faker::Lorem.sentence}
+    content { Faker::Lorem.sentence }
   end
 end
--- spec/models/micropost_spec.rb
+++ spec/models/micropost_spec.rb
@@ -9,26 +9,27 @@ RSpec.describe Micropost, type: :model do
   context '投稿が上手くいくとき' do
     it '正しく情報を入力できれば投稿できる' do
       expect(@micropost).to be_valid
-     end
     end
-  
+  end
+
   context '投稿がうまくいかないとき' do
-  it "contentが空だと投稿できない" do
-   @micropost.content = nil
-   @micropost.valid?
-   expect(@micropost.errors.full_messages).to include( "Content can't be blank")
-  end
-  it "contentが141文字以上だと投稿できない" do
-    #141字のダミーデータ
-    @micropost.content =  "この文章はダミーです。文字の大きさ、量、字間、行間等を確認するために入れています。この文章はダミーです。文字の大きさ、量、字間、行間等を確認するために入れています。この文章はダミーです。文字の大きさ、量、字間、行間等を確認するために入れています。この文章はダミーです。文字の大きさ、"
-    @micropost.valid?
-    expect(@micropost.errors.full_messages).to include( "Content is too long (maximum is 140 characters)")
-  end
-  it "userに紐付いていないと投稿できない" do
-    @micropost.user =nil
-    @micropost.valid?
-    expect(@micropost.errors.full_messages).to include("User must exist")
-  end
-  # expect(@user).not_to be_valid
+    it 'contentが空だと投稿できない' do
+      @micropost.content = nil
+      @micropost.valid?
+      expect(@micropost.errors.full_messages).to include("Content can't be blank")
+    end
+    it 'contentが141文字以上だと投稿できない' do
+      # 141字のダミーデータ
+      @micropost.content = 'この文章はダミーです。文字の大きさ、量、字間、行間等を確認するために入れています。この文章はダミーです。文字の大きさ、量、字間、
+      行間等を確認するために入れています。この文章はダミーです。文字の大きさ、量、字間、行間等を確認するために入れています。この文章はダミーです。文字の大きさは'
+      @micropost.valid?
+      expect(@micropost.errors.full_messages).to include('Content is too long (maximum is 140 characters)')
+    end
+    it 'userに紐付いていないと投稿できない' do
+      @micropost.user = nil
+      @micropost.valid?
+      expect(@micropost.errors.full_messages).to include('User must exist')
+    end
+    # expect(@user).not_to be_valid
   end
 end
